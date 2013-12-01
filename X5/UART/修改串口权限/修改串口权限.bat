9e970a5cae4d2136cf1f28b76e362738a81554d6
chengzhiqiang <czqchengzhiqiang@126.com>
2013-12-01 15:29:40 +0800

    x5串口权限修改脚本

---
+++ X5/UART/修改串口权限/修改串口权限.bat
@@ -0,0 +1,7 @@
+cd ./platform-tools
+adb shell "chmod 666 /dev/s3c2410_serial0"
+adb shell "chmod 666 /dev/s3c2410_serial1"
+adb shell "chmod 666 /dev/s3c2410_serial2"
+adb shell "chmod 666 /dev/s3c2410_serial3"
+adb shell "chmod 666 /dev/ttyGS0"
+pause
